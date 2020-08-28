b61586512752f0b1e131cc3c88779d1a6e05411b
Keshara Piyumal <keshara.piyumal@oulook.com>
2020-08-28 13:15:36 +0530

    updated

--- src/misc/mailer.js
+++ src/misc/mailer.js
@@ -1,5 +1,5 @@
 const elasticEmail = require('elasticemail');
-const elasticMailer = require('./mailerCredentials');
+const elasticMailer = require('../auth/mailerCredentials');
 const client = elasticEmail.createClient({
   username: elasticMailer.USER_NAME,
   apiKey: elasticMailer.KEY
